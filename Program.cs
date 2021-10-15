b1b4136a5602c03a4577e30da23d1bad4b1aa878
Danny Hinds <dhinds1@my.wctc.edu>
2021-10-15 15:27:57 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetTicketSysTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
